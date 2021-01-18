f6ae7e49b0e2d6be341b052c36c2c984ce3164d9
eijoac <eijoac@hotmail.com>
2021-01-18 13:59:49 -0500

    modified comments in dl demo

--- dl_hello_world.R
+++ dl_hello_world.R
@@ -2,23 +2,25 @@
 # load the library
 library(keras)
 
-# 1. load data and rescale the x variable
+# 1. load data
 mnist <- dataset_mnist()
+
+# 2. Re-scale the x variable
 mnist$train$x <- mnist$train$x/255
 mnist$test$x <- mnist$test$x/255
 
-# 2. modeling
-# 2.1 specify a model
+# 3. modeling
+# 3.1 specify a model
 model <- keras_model_sequential() %>% 
   layer_flatten(input_shape = c(28, 28)) %>% 
   layer_dense(units = 128, activation = "relu") %>% 
   layer_dropout(0.2) %>% 
   layer_dense(10, activation = "softmax")
 
-# 2.2 show the model summary
+# 3.2 show the model summary
 summary(model)
 
-# 2.3 compile the model
+# 3.3 compile the model
 model %>% 
   compile(
     loss = "sparse_categorical_crossentropy",
@@ -26,7 +28,7 @@ model %>%
     metrics = "accuracy"
   )
 
-# 2.4 fit the model
+# 3.4 fit the model
 model %>% 
   fit(
     x = mnist$train$x, y = mnist$train$y,
@@ -35,11 +37,11 @@ model %>%
     verbose = 2
   )
 
-# 3. predict using the model
+# 4. predict using the model
 predictions <- predict(model, mnist$test$x)
 head(predictions, 2)
 
-# 4. evaluate the model
+# 5. evaluate the model / report model performance
 model %>% 
   evaluate(mnist$test$x, mnist$test$y, verbose = 0)
 
